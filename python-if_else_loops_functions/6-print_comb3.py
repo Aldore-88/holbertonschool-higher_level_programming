e4c19620f5a0795fe94736a5d0ca275bbfe3dc59
Bevan Tan <btan400@gmail.com>
2025-05-08 17:06:20 +1000

    6-print_comb3.py

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 for tens_digit in range(0, 10):
     for ones_digit in range(0, 10):
-        total = tens_digit * 10 + ones_digit
         if (tens_digit < ones_digit):
-            print("{:02d}".format(total))
+            if (tens_digit == 8 and ones_digit == 9):
+                print("{}{}".format(tens_digit, ones_digit))
+            else:
+                print("{}{}".format(tens_digit, ones_digit), end=", ")
