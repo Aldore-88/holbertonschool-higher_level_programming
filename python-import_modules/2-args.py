9067019b383f450ad7d6d4744965f11875a38100
Bevan Tan <btan400@gmail.com>
2025-05-09 23:13:42 +1000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 import sys
-
-if len(sys.argv) == 1:
-    print("0: arguments.")
-elif len(sys.argv) > 1:
-    print(len(sys.argv), end=": arguments:\n")
-    for i in range(len(sys.argv)):
-        if i > 0:
-            print(i, end=": ")
-            print(sys.argv[i])
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        print("0: arguments.")
+    elif len(sys.argv) > 1:
+        print(len(sys.argv), end=": arguments:\n")
+        for i in range(len(sys.argv)):
+            if i > 0:
+                print(i, end=": ")
+                print(sys.argv[i])
