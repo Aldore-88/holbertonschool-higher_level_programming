5ccb589981335ff9a58fe91a25834a83b3db9b42
Bevan Tan <btan400@gmail.com>
2025-05-09 23:22:52 +1000

    2-args.py

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 import sys
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("0: arguments.")
+        print("0 arguments.")
     elif len(sys.argv) > 1:
         if len(sys.argv) == 2:
             print("1 argument:")
