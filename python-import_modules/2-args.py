6fff43ff7b74eec68087a511453ee81bbb2efefb
Bevan Tan <btan400@gmail.com>
2025-05-09 23:20:15 +1000

    2-args.py removed :

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
         print("0: arguments.")
     elif len(sys.argv) > 1:
         if len(sys.argv) == 2:
-            print("1: argument:")
+            print("1 argument:")
         elif len(sys.argv) > 2:
             print(len(sys.argv), end=": arguments:\n")
         for i in range(len(sys.argv)):
