5c94ac1a0e49dac6ae2cc50918262236b65b3cb8
Bevan Tan <btan400@gmail.com>
2025-05-09 23:10:47 +1000

    2-args.py added arguments count

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -4,6 +4,7 @@ import sys
 if len(sys.argv) == 1:
     print("0: arguments.")
 elif len(sys.argv) > 1:
+    print(len(sys.argv), end=": arguments:\n")
     for i in range(len(sys.argv)):
         if i > 0:
             print(i, end=": ")
