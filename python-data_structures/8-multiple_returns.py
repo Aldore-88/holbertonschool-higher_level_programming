4f76409978e49ef22cbd8d1763343b3b7c19fb11
Bevan Tan <btan400@gmail.com>
2025-05-17 12:43:43 +1000

    8-multiple_returns.py

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
 def multiple_returns(input_sentence):
+    if input_sentence == "":
+        return (0, None)
     output_tuple = (len(input_sentence), input_sentence[0])
     return (output_tuple)
