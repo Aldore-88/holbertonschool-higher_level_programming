01896c177eba834417ec8bc87bd4a4f221688b4a
Bevan Tan <btan400@gmail.com>
2025-05-18 10:07:22 +1000

    2-matrix_divided.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
-# Adds integers together
 """Interger addition"""
+
 
 def add_integer(a, b=98):
     """
-    Return the addition
+    Args:
     a float
     b float
 
-    Raise: TypeError if a or b are non-integers
+    Return:
+    Sum of the 2 integers
+
+    Raise:
+    TypeError if a or b are non-integers
     """
 
     if type(a) is float:
@@ -16,9 +20,9 @@ def add_integer(a, b=98):
     if type(b) is float:
         b = int(b)
     if type(a) is not int:
-        raise TypeError ("a must be an integer")
+        raise TypeError("a must be an integer")
     if type(b) is not int:
-        raise TypeError ("b must be an integer")
+        raise TypeError("b must be an integer")
     else:
         total = a + b
         return (total)
