6c38128cce154d11c979245b009201b0631d0930
Bevan Tan <btan400@gmail.com>
2025-05-18 10:05:22 +1000

    0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,6 +1,16 @@
 #!/usr/bin/python3
+# Adds integers together
+"""Interger addition"""
+
 def add_integer(a, b=98):
-    
+    """
+    Return the addition
+    a float
+    b float
+
+    Raise: TypeError if a or b are non-integers
+    """
+
     if type(a) is float:
         a = int(a)
     if type(b) is float:
