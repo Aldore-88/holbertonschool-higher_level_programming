1631bd09ff4c1d7d0f9f6ff4e046498bf6747747
Bevan Tan <btan400@gmail.com>
2025-05-20 17:41:33 +1000

    1-search_replace.py

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+#def square_matrix_simple(matrix=[]):
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+for i in list(matrix):
+    new_matrix = i
+
+#new_matrix = list(matrix[2])
+
+print(new_matrix)
+print(matrix)
