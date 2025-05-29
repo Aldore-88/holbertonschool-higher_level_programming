45717ea77a608053abda72d3707f75636da2afee
Bevan Tan <btan400@gmail.com>
2025-05-29 11:05:52 +1000

    .format()

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -7,5 +7,5 @@ def safe_print_division(a, b):
     except ZeroDivisionError:
         total = None
     finally:
-        print(f"Inside result:{total}")
+        print("Inside result:{}".format(total))
         return (total)
