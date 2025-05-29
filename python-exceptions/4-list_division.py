89670c472517346f4ca625b86e051d9231ea038d
Bevan Tan <btan400@gmail.com>
2025-05-29 11:53:27 +1000

    result = 0 for exceptions

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -9,10 +9,13 @@ def list_division(my_list_1, my_list_2, list_length):
         try:
             result = my_list_1[i] / my_list_2[i]
         except IndexError:
+            result = 0
             print("out of range")
         except (TypeError, NameError):
+            result = 0
             print("wrong type")
         except ZeroDivisionError:
+            result = 0
             print("division by 0")
         finally:  # finally overrides all other excepts
             total_list.append(result)
