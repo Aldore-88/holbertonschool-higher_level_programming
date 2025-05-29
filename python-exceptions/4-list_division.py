d9ac2dbed64ecf9779d2641ffc8f26d6e62bcd26
Bevan Tan <btan400@gmail.com>
2025-05-29 11:51:26 +1000

    4-list_division.py

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    # my_list_1 = [10, 8, 4]
+    # my_list_2 = [2, 4, 0, "H", 5]
+    list_length = max(len(my_list_1), len(my_list_2))
+    total_list = []
+
+    for i in range(list_length):
+        try:
+            result = my_list_1[i] / my_list_2[i]
+        except IndexError:
+            print("out of range")
+        except (TypeError, NameError):
+            print("wrong type")
+        except ZeroDivisionError:
+            print("division by 0")
+        finally:  # finally overrides all other excepts
+            total_list.append(result)
+    return (total_list)
