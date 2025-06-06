ef823220f7704fc733d5f69bcbc14e5d1546def3
Bevan Tan <btan400@gmail.com>
2025-06-06 21:38:56 +1000

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -12,11 +12,16 @@ raise a ValueError exception with the message "size must be >= 0"
 
 
 class Square:
-    """class documentation"""
+    """
+    class documentation
+    
+    raise TypeError when not int
+    riase ValueError when less than 0
+    """
     def __init__(self, size=0):
 
         if type(size) is not int:
             raise TypeError("size must be int")
-        elif size < 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
