bbe567f8da90754cdcc31d436de845d02a65c2d8
Bevan Tan <btan400@gmail.com>
2025-06-06 22:23:16 +1000

    docuemnt

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,6 +1,24 @@
 #!/usr/bin/python3
+"""
+Private instance attribute: size
+Instantiation with optional size: def __init__(self, size=0):
+size must be an integer, otherwise raise a
+TypeError exception with the message size must be an integer
+
+if size is less than 0, raise a ValueError
+exception with the message size must be >= 0
+
+Public instance method: def area(self): that returns the current square area
+"""
+
+
 class Square:
-    def __init__(self,size=0):
+    """
+    __init__ initialises square
+    area(self) returns __size^2 for area of square
+    """
+
+    def __init__(self, size=0):
 
         if type(size) is not int:
             raise TypeError("size must be an integer")
