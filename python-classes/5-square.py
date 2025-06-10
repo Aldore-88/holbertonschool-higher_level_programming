42e43853b627241a19f4a9b9f6939bdc0dc7ee35
Bevan Tan <btan400@gmail.com>
2025-06-10 18:11:04 +1000

    5-square.py

---
+++ python-classes/5-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""4. Access and update private attribute"""
+
+
+class Square:
+    """
+    Declare Square class
+    _size(int) - private attibute
+    __init__ initiate square
+    """
+    def __init__(self, size=0):
+        """initialse new square size"""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >=0")
+        self.__size = size
+        """print("init running")"""
+
+    @property
+    def size(self):
+        """getter"""
+        """print("getter running")"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter size private??"""
+        """print("setter running")"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >=0")
+        self.__size = value
+
+    def area(self):
+        """area of square"""
+        squared = (self.__size ** 2)
+        """print("area running")"""
+        return squared
+    
+    def my_print(self):
+        """printing square #"""
+        if self.__size == 0:
+            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end = "")
+                print()
