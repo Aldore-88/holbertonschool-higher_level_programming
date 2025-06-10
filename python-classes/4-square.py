616f6299c6ea5a252b4574600a89367dd608c8e4
Bevan Tan <btan400@gmail.com>
2025-06-10 16:47:51 +1000

    4-square.py

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,7 +1,18 @@
 #!/usr/bin/python3
 
 class Square:
+    """
+    Declare Square class
+    
+    _size(int) - private attibute
+    __init__ initiate square
+
+    """
     def __init__(self, size=0):
+        """
+        initialse new square
+        
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -10,11 +21,17 @@ class Square:
 
     @property
     def size(self):
+        """
+        getter size
+        """
         sizeB = self.sizeA
         return sizeB
 
     @size.setter
     def size(self, size):
+        """
+        setter
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
@@ -22,5 +39,8 @@ class Square:
         self.__size = size
 
     def area(self):
+        """
+        area of square
+        """
         squared = (self.sizeA ** 2)
         return squared
