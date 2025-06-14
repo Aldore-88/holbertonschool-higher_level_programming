2ece8f2c6158084d0284a413f50aedf973b06c69
Bevan Tan <btan400@gmail.com>
2025-06-14 14:26:37 +1000

    3-is_kind_of_class.py

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+3.Same class or inherit from
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    isinstance() built in function in python
+    return True if obj is obj is of a specific type a_class
+    """
+    return isinstance(obj, a_class)
