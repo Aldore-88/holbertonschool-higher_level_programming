4e36d77ee3f77f891114d82f02856d9ae35c04c7
Bevan Tan <btan400@gmail.com>
2025-06-14 14:15:56 +1000

    2-is_same_class.py

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+2. Exact same object
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    checking obj type, and comparing against a_class
+
+    return True if obj is int
+    """
+    if type(obj) == a_class:
+        return True
+    else:
+        return False
