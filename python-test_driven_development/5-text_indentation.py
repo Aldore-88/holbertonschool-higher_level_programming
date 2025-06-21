b3e437e7e2765ae3a61e1fdf9debc98348d9613c
Bevan Tan <btan400@gmail.com>
2025-06-21 19:10:01 +1100

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -14,9 +14,23 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    for delimeter in "?:.":
-        words = (delimeter + "\n\n").join(
-                [index.strip(" ") for index in words.split(delimeter)])
+    i = 0
+    
+    while i < len(text):
+        if text[i] in ".?:":
+            print(text[i])
+            print()
+            i += 1
+            
+            while i < len(text) and text[i] == ' ':
+                i += 1
+                
+        elif text[i] == ' ' and (i == 0 or i == len(text) - 1):
+            i += 1
+            
+        else:
+            print(text[i], end="")
+            i += 1
 
 
 if __name__ == "__main__":
