1355e6c67c3189ae50d6d62ef727e7c9a6e4863b
Bevan Tan <btan400@gmail.com>
2025-06-21 18:15:21 +1100

    5-text_indentation.py

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""5-text indent"""
+
+
+def text_indentation(text):
+    """Print text with 2 newlines after each '.', '?', and ':'.
+
+    Args:
+        text (str): The text to print.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for delimeter in "?:.":
+        words = (delimeter + "\n\n").join(
+                [index.strip(" ") for index in words.split(delimeter)])
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/5-text_indentation.txt")
