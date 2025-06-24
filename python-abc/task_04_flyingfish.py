e938b2eecfc745a0d28a91b3e145a6aad84a2084
Bevan Tan <btan400@gmail.com>
2025-06-24 12:13:15 +1000

    task_04_flyingfish.py

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""4. The Enigmatic FlyingFish - Exploring Multiple Inheritance"""
+
+
+class Fish:
+    """fish class"""
+    def swim(self):
+        """method swim of fish"""
+        print("The fish is swimming")
+
+    def  habitat(self):
+        """method habititat of fish"""
+        print("The fish lives in water")
+
+
+class Bird:
+    """bird class"""
+    def fly(self):
+        """method fly of bird"""
+        print("The bird is flying")
+
+    def  habitat(self):
+        """method habitat of bird"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """flying fish class"""
+    def fly(self):
+        """method fly of flyingfish - overriding parent class fly mehtod"""
+        print("The flying fish is soaring!")
+    
+    def swim(self):
+        """method swim of flying fish - overriding parent class swim method"""
+        print("The flying fish is swimming")
+
+    def habitat(self):
+        """method habitiat of flyingfish - overriding parnt classes habitat methods"""
+        print("The flying fish loves both water and the sky")
+
+    # def __init__(self):
+    #     self.habitat()
+    #     self.swim()
+    #     self.fly()
