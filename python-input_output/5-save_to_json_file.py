593b2441931431fdb2b74a97f8eb056a2c57f0c6
Bevan Tan <btan400@gmail.com>
2025-06-26 18:15:37 +1000

    dump not dumps??

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,6 +7,6 @@ def save_to_json_file(my_obj, filename):
     """
     writes object to a text file using json
     """
-    input_info = json.dumps(my_obj)
+    input_info = json.dump(my_obj)
     with open(filename, "w") as file:
         file.write(input_info)
