8450810d85eaf1abda73372e92b6544b7c27f87c
Bevan Tan <btan400@gmail.com>
2025-06-26 22:25:55 +1000

    6-load_from_json_file.py

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -7,6 +7,7 @@ def save_to_json_file(my_obj, filename):
     """
     writes object to a text file using json
     needed to input straight to the file.write function
+    dump(s) the s returns a string
     """
     with open(filename, "w") as file:
         file.write(json.dumps(my_obj))
