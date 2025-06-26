61ee7fff396ef18b7d20fc84d4c1c3d396588c8f
Bevan Tan <btan400@gmail.com>
2025-06-26 11:38:09 +1000

    0- 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""0.Read file"""
 
 def read_file(filename=""):
-    with open(filename, "r") as file:
-        read_data = file.read()
-        print(read_data)
+    """
+    need to use with method
+    -> with takes care of closing files
+    """
+
+    with open(filename) as file:
+        print(file.read())
+
+    # f = open(filename)
+    # print(f.read())
