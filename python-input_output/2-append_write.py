7d8a9c9269ac64429edd8fde5626454322cae4cd
Bevan Tan <btan400@gmail.com>
2025-06-26 12:35:20 +1000

    2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""2.Append to a file"""
+
 
 def append_write(filename="", text=""):
+    """
+    Append to a file
+    return file with appended text
+    """
     with open(filename, "a") as file:
         return file.write(text)
