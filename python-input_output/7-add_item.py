66933a5c6beac076e6f53e598c4746c1355d96e2
Bevan Tan <btan400@gmail.com>
2025-06-30 11:16:24 +1000

    7-add_item.py

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
+from sys import argv
 """7-Load, add, save"""
 """
 importing argv from sys library
 to add arguments to python list
 """
-from sys import argv
 
 save_file = __import__("5-save_to_json_file").save_to_json_file
 load_file = __import__("6-load_from_json_file").load_from_json_file
