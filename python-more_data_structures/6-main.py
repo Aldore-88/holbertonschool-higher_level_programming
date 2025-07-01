4a72f3421ee8db9c95dac45baeb9ba6c73372d77
Bevan Tan <btan400@gmail.com>
2025-07-01 17:51:21 +1000

    task-00_basic_serialization

--- python-more_data_structures/6-main.py
+++ python-more_data_structures/6-main.py
@@ -2,4 +2,5 @@
 print_sorted_dictionary = __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
 a_dictionary = { 'language': "C", 'Number': 89, 'track': "Low level", 'ids': [1, 2, 3] }
+#a_dictionary = {'a':"e", 'B':"d", 'c':"c", 'd':"b", 'E':"a"}
 print_sorted_dictionary(a_dictionary)
