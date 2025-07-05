be27f0ee733f8e1ac45302506bb829682ab8e6fa
Bevan Tan <btan400@gmail.com>
2025-07-05 21:59:20 +1000

    task_02_csv.py exception

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,9 +3,14 @@ import csv
 import json
 
 def convert_csv_to_json(csv_file):
+    try:
+        with open("data.csv", mode="r", newline="") as csvfile:
+            csv_data = list(csv.DictReader(csvfile))
+        
+        with open("data.json", "w") as jsonfile:
+            json.dump(csv_data, jsonfile, indent=4)
 
-    with open("data.csv", mode="r", newline="") as csvfile:
-        csv_data = list(csv.DictReader(csvfile))
-    
-    with open("data.json", "w") as jsonfile:
-        json.dump(csv_data, jsonfile)
+        return True
+    except FileExistsError:
+        print(f"Error: File {csvfile} not found")
+
