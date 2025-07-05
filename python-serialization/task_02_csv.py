f1508394b6e8cf5c163bc8c7314de99c5a5c8d45
Bevan Tan <btan400@gmail.com>
2025-07-05 21:51:25 +1000

    task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -3,3 +3,9 @@ import csv
 import json
 
 def convert_csv_to_json(csv_file):
+
+    with open("data.csv", mode="r", newline="") as csvfile:
+        csv_data = list(csv.DictReader(csvfile))
+    
+    with open("data.json", "w") as jsonfile:
+        json.dump(csv_data, jsonfile)
