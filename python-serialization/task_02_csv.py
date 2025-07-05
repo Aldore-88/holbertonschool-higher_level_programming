d41952bb07abc1293002d53b7d451ed389f32d8c
Bevan Tan <btan400@gmail.com>
2025-07-05 22:06:56 +1000

    task_02_csv.py

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -6,11 +6,12 @@ def convert_csv_to_json(csv_file):
     try:
         with open("data.csv", mode="r", newline="") as csvfile:
             csv_data = list(csv.DictReader(csvfile))
-        
+
         with open("data.json", "w") as jsonfile:
             json.dump(csv_data, jsonfile, indent=4)
 
         return True
+
     except FileExistsError:
         print(f"Error: File {csvfile} not found")
-
+        return False
