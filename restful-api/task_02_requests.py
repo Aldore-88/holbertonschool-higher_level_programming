0bdfb915307954750d039f5247cb9ab9f882fddd
Bevan Tan <btan400@gmail.com>
2025-07-07 17:10:18 +1000

    task_02_requests.py

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 import requests
+import csv
 
-def fetch_and_print_posts(input_url):
+
+def fetch_and_print_posts():
+    input_url = "https://jsonplaceholder.typicode.com/posts"
+
     data = requests.get(input_url)
     print(data.text)
     # print(data.text)
-    # print(data.json()) #data inside of json
+    print(f"json data {data.json()}") #data inside of json
     # print(data.json) #refers to the json object with the data
     # print()
 
@@ -19,8 +23,33 @@ def fetch_and_print_posts(input_url):
         elif isinstance(data_json, dict):
             print(data_json["title"])
 
-def fetch_and_save_posts(input_url):
+def fetch_and_save_posts():
+    input_url = "https://jsonplaceholder.typicode.com/posts"
+    
     data = requests.get(input_url)
-        
+    if data.status_code == 200:
+        print("Request Success")
+    
+    all_data = []
 
-fetch_and_print_posts("https://jsonplaceholder.typicode.com/todos/1")
+    for item in data.json():
+        print(item)
+        # print(item['id'])
+        item_list = {
+            'id' : item['id'],
+            'title' : item['title'],
+            'body' : item['body']
+        }
+        all_data.append(item_list) #everything into one big list
+    # print(all_data)
+
+    csv_file = "posts.csv"
+    with open(csv_file, "w"):
+        values = ["id", "title", "body"]
+        writer = csv.DictWriter(csv_file, values)
+        writer.writeheader()
+        writer.writerows(all_data)
+
+
+fetch_and_print_posts()
+fetch_and_save_posts()
