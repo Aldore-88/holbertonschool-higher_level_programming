84509b4520f54c7b8c219dfb4c7cac8378bc7fd6
Bevan Tan <btan400@gmail.com>
2025-07-09 21:07:31 +1000

    task_04_flask.py - user not found

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -24,15 +24,20 @@ def status():
 
 @app.route('/users/<username>') #this is the input to check "username"
 def get_user(username):
-    return (jsonify(users.get(username)))
+    user = users.get(username)
+    if user:
+        return (jsonify(user))
+    else:
+        return ({"error": "User not found"}, 404)
 
-@app.route("/add_user", methods=["POST"])
+@app.route("/add_user", methods=["POST"]) #signifies the POST request
 def add_data():
     user_data = request.get_json() #gets the json data that is input from client
     user_id = user_data.get("username") #reads the user_data and gets "username"s
 
     if not user_id:
-        return (jsonify({"error":"Username is required"}), 400)
+        return (jsonify({"error": "Username is required"}), 400)
+
     users[user_id] = user_data #saves the "user_data" under the specific "user_id"
 
     return (jsonify({"message": "User added", "user": user_data}), 201)
