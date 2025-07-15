6d1fd57feef9fa134375163e5a87260cc45a71cc
Bevan Tan <btan400@gmail.com>
2025-07-15 17:36:05 +1000

    0-privileges.sql

---
+++ SQL_more_queries/0-privileges.sql
@@ -0,0 +1,9 @@
+-- 0.My privileges
+-- Write a script that lists all privileges of the MySQL users user_0d_1 and user_0d_2 on your server (in localhost).
+
+-- run: echo "CREATE USER 'user_0d_1'@'localhost';" |  mysql -hlocalhost -uroot -p
+-- then: echo "GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';" |  mysql -hlocalhost -uroot -p
+-- execute: cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
