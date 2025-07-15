ef655c54ab12aa006df672518b41d9d387f1bb55
Bevan Tan <btan400@gmail.com>
2025-07-15 17:55:26 +1000

    must specify the server of the user

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,11 @@
+-- 2.Read user
+-- Write a script that creates the database hbtn_0d_2 and the user user_0d_2.
+
+-- execute: cat 2-create_read_user.sql | mysql -hlocalhost -uroot -p
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS "user_0d_2"@"localhost" IDENTIFIED BY "user_0d_2_pwd";
+GRANT SELECT ON *.* TO "user_0d_2"@"localhost";
+
+-- have to specify the server of the user!!
