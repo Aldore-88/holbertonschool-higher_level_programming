d4cdedbb66df037e1502b66729a6749c12af0006
Bevan Tan <btan400@gmail.com>
2025-07-15 16:32:53 +1000

    10-top_score.sql

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,9 @@
+-- 10.List by best
+-- Write a script that lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- execute: cat 10-top_score.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+SELECT * FROM second_table
+ORDER by score DESC;
+
+--https://www.w3schools.com/sql/sql_orderby.asp
