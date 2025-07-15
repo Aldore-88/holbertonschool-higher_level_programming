c7d38855be660344dccbda78c4d505b2d23820d6
Bevan Tan <btan400@gmail.com>
2025-07-15 17:01:07 +1000

    15-groups.sql

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,10 @@
+-- 15.Number by score
+-- Write a script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+-- execute: cat 15-groups.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+
+SELECT score, COUNT(score) AS number
+FROM second_table
+GROUP BY score;
+
+-- https://www.w3schools.com/sql/sql_groupby.asp
