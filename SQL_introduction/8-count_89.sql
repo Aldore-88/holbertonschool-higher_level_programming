c827068276d56433aeefdceaeeda42577c50cd81
Bevan Tan <btan400@gmail.com>
2025-07-15 15:53:51 +1000

    8-count_89.sql

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,11 @@
+-- 8.Count 89
+-- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
+
+-- execute: cat 8-count_89.sql | mysql -hlocalhost -uroot -p hbtn_0c_0 | tail -1
+
+USE hbtn_0c_0;
+SELECT COUNT(id) 
+FROM first_table
+WHERE 89;
+
+-- https://www.w3schools.com/sql/sql_count.asp
