64af31677ffc7cfeb9bf2d0868b05dab47042fc5
Bevan Tan <btan400@gmail.com>
2025-07-16 17:08:29 +1000

    8-cities_of_california_subquery.sql

---
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -0,0 +1,11 @@
+-- 8.Cities of California
+-- Write a script that lists all the cities of California that can be found in the database hbtn_0d_usa.
+
+-- echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- echo 'SELECT * FROM cities;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- cat 8-cities_of_california_subquery.sql | mysql -hlocalhost -uroot -p hbtn_0d_usa
+
+SELECT * FROM cities
+WHERE state_id = (
+    SELECT id FROM states WHERE name = "California"
+);
