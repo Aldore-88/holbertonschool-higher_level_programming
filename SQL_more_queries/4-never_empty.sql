d02d2c339d3164d8a948c3f1652325d2d6a23cf1
Bevan Tan <btan400@gmail.com>
2025-07-16 14:51:48 +1000

    4-never_empty.sql

---
+++ SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,14 @@
+-- 4.ID cant be null
+-- Write a script that creates the table id_not_null on your MySQL server.
+
+-- execute: cat 4-never_empty.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'INSERT INTO id_not_null (id, name) VALUES (89, "Best School");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'INSERT INTO id_not_null (name) VALUES ("Best");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+
+CREATE TABLE IF NOT EXISTS id_not_null(
+    id INT,
+    name VARCHAR(256)
+);
+
