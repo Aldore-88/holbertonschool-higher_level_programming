1a5075222984f7f066cbb4da2708053128d6e7f2
Bevan Tan <btan400@gmail.com>
2025-07-16 15:34:08 +1000

    6-states.sql

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -6,3 +6,13 @@
 -- echo 'INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(
+    id INT UNIQUE AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+)
+
+-- https://www.w3schools.com/sql/sql_primarykey.ASP
+-- https://www.w3schools.com/sql/sql_autoincrement.asp
