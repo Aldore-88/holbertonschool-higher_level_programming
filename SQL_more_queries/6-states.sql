73d150ea8dd4b0c07cc89060b4bae0244a299bdd
Bevan Tan <btan400@gmail.com>
2025-07-16 15:24:44 +1000

    5-unique_id.sql

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- 6.States table
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
+
+
+-- cat 6-states.sql | mysql -hlocalhost -uroot -p
+-- echo 'INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");' | mysql -hlocalhost -uroot -p hbtn_0d_usa
+-- echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
+
