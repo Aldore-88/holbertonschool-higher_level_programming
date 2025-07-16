d41f2cc703af420de1ce1746f488d5f2ca478769
Bevan Tan <btan400@gmail.com>
2025-07-16 15:16:44 +1000

    5-unique_id.sql

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -9,7 +9,8 @@ echo 'INSERT INTO unique_id (id, name) VALUES (89, "Best");' | mysql -hlocalhost
 echo 'SELECT * FROM unique_id;' | mysql -hlocalhost -uroot -p hbtn_0d_2
 */
 
-CREATE TABLE IF NOT EXISTS unique_id(
+CREATE TABLE IF NOT EXISTS unique_id
+(
     id INT DEFAULT 1 UNIQUE,
     name VARCHAR(256)
 );
