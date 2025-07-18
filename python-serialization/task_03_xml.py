900b4b5445f23abf2881586f3894cdab3e0e441b
Bevan Tan <btan400@gmail.com>
2025-07-18 21:14:16 +1000

    task_03_xml.py

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 import xml.etree.ElementTree as ET
-import json
 
 def serialize_to_xml(dictionary, filename):
-    tree = ET.parse('movies.xml')
+    tree = ET.parse(dictionary)
     root = tree.getroot()
-    
+    print(tree)
+    print(root)
+    print(filename)
+
+sample_dict = {
+    'name': 'John',
+    'age': '28',
+    'city': 'New York'
+}
+
+xml_file = "data.xml"
+serialize_to_xml(sample_dict, xml_file)
