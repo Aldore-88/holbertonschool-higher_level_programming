ef66e181f887ea43ff827af6955df6012327d0f2
Bevan Tan <btan400@gmail.com>
2025-07-20 12:03:52 +1000

    10-genre_id_by_show.sql

---
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,10 @@
+-- 10.Genre ID by show
+-- Import the database dump from hbtn_0d_tvshows to your MySQL server: download
+
+-- cat 10-genre_id_by_show.sql | mysql -hlocalhost -uroot -p hbtn_0d_tvshows
+
+SELECT tv_shows.title, tv_show_genres.genre_id 
+
+FROM tv_shows
+INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
