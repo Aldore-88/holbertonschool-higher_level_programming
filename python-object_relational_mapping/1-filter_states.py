2f55a66b56d42bf6a6a15c545b4d252cfe1b9472
Bevan Tan <btan400@gmail.com>
2025-07-25 20:38:37 +1000

    1-filter_states.py

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""1.Filter States"""
+
+import MySQLdb
+import sys
+
+def filter_states_by_N(username, password, database):
+    """filtering by states name starting with N"""
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cur=db.cursor()
+    # SQL command
+    cur.execute("SELECT * FROM states WHERE name LIKE 'n%' ORDER BY id ASC;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
+
+if __name__ == "__main__":
+    """stops execute when imported"""
+    filter_states_by_N(sys.argv[1], sys.argv[2], sys.argv[3])
