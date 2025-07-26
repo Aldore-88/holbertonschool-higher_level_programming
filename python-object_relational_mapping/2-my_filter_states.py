bf42726aac634aa310d71fae9e5573df0a658a95
Bevan Tan <btan400@gmail.com>
2025-07-26 16:04:51 +1000

    2-my_filter_states.py

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+import MySQLdb
+import sys
+
+
+def filter_by_user_input(username, password, database, input_filter):
+    """filter database by user input"""
+    db_connect = MySQLdb.connect(
+        host="localhost",
+        user=username,
+        passwd=password,
+        port=3306,
+        db=database
+    )
+
+    cur = db_connect.cursor()
+
+    cur.execute(
+        "SELECT * FROM states "
+        "WHERE name = '{}' "
+        "ORDER BY id ASC;".format(input_filter)
+    )
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db_connect.close()
+
+if __name__ == "__main__":
+    """stops execute when imported"""
+    filter_by_user_input(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
