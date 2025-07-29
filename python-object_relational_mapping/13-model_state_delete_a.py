2664a07fddab546521d7c344517a2977badb75c9
Bevan Tan <btan400@gmail.com>
2025-07-29 19:43:48 +1000

    13-model_state_delete_a.py

---
+++ python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""13.Delete states"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)  # database connection creator
+from sqlalchemy.orm import sessionmaker  # session factory creator
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """creates tables if it doesnt exist from Base"""
+    Base.metadata.create_all(engine)
+    """creates session factory"""
+    """not a session itself"""
+    Session = sessionmaker(bind=engine)
+    """creates actual session?? why??"""
+    session = Session()
+
+    """deletes states containing a"""
+    states = session.query(State).filter(State.name.contains("a")).all()
+    for state in states:
+        session.delete(state)
+
+    """need to commit changes"""
+    session.commit()
+
+    """close session"""
+    session.close()
