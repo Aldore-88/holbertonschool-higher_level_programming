652ea3eef41a12bb4a6c852a8096fb390d6eade2
Bevan Tan <btan400@gmail.com>
2025-07-29 19:03:55 +1000

    10-model_state_my_get.py

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""10.Get state"""
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)  # database connection creator
+from sqlalchemy.orm import sessionmaker  # session factory creator
+from sqlalchemy import (create_engine)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]))
+
+    """creates tables if it doesnt exist from Base"""
+    Base.metadata.create_all(engine)
+    """creates session factory"""
+    """not a session itself"""
+    Session = sessionmaker(bind=engine)
+    """creates actual session?? why??"""
+    session = Session()
+
+    """searches state for the input name"""
+    state_search = sys.argv[4]
+    states = session.query(State).order_by(State.id).filter(State.name == state_search).all()
+    if states is None:
+        print("Not found")
+    else:
+        for state in states:
+            print(state.id)
+
+    """close session"""
+    session.close()
