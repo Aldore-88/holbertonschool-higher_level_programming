9de2c6cbd035f9ca6c4b2a506cef1a764b850ad8
Bevan Tan <btan400@gmail.com>
2025-07-29 17:41:30 +1000

    6-model_state

---
+++ python-object_relational_mapping/7-model_state_fetch_all.sql
@@ -0,0 +1,4 @@
+-- Insert states
+INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas"), ("New York"), ("Nevada");
+
+-- cat 7-model_state_fetch_all.sql | mysql -uroot -p hbtn_0e_6_usa
