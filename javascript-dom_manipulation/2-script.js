56bcaf7f0037650c3acb05f4e9546ba95de25abf
Bevan Tan <btan400@gmail.com>
2025-08-06 23:37:38 +1000

    2-script.js

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,7 +1,7 @@
 const red_header_element = document.getElementById("red_header");
-//document.querySelector('#red_header')
+
 const header_element = document.querySelector("header");
 
 red_header_element.addEventListener('click', function() {
-    header_element.style.color = "#FF0000"
+    header_element.classList.add("red")
 });
