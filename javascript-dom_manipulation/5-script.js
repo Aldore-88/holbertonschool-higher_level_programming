d1397488a3cacadc7835c768fb85553301229b77
Bevan Tan <btan400@gmail.com>
2025-08-08 22:34:30 +1000

    console.logging

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,6 +1,11 @@
+console.log("script started")
 const update_header_element = document.getElementById("update_header");
 const header_element = document.querySelector("header")
 
 update_header_element.addEventListener('click', function() {
     header_element.innerHTML = "New Header!!!";
-})
+
+    console.log(header_element);
+    console.log("woo");
+})  
+console.log("event still running");
