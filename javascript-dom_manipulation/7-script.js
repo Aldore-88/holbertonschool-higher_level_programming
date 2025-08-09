60161514190521359108216b70b05f2a47941701
Bevan Tan <btan400@gmail.com>
2025-08-09 18:42:53 +1000

    comment

--- javascript-dom_manipulation/7-script.js
+++ javascript-dom_manipulation/7-script.js
@@ -1,3 +1,8 @@
+/*
+7.Star Wars movies
+Write a JavaScript script that fetches and lists the title for all movies by using this URL: https://swapi-api.hbtn.io/api/films/?format=json
+*/
+
 const URL_data = "https://swapi-api.hbtn.io/api/films/?format=json";
 const list_element = document.getElementById("list_movies");
 
@@ -6,13 +11,13 @@ fetch(URL_data)
         return response.json();
     })
     .then((data_json) => {
-        console.log("received:", data_json.results);
+        // console.log("received:", data_json.results);
         const title_count = data_json.results.length;
-        console.log(title_count);
+        // console.log(title_count);
 
         for (let i=0; i < title_count; i++){
 
-            let new_title = document.createElement('li');
+            const new_title = document.createElement('li');
             new_title.textContent = data_json.results[i].title;
 
             list_element.appendChild(new_title);
