383950af60d205a8b5ea8705b235567e46cbd099
Bevan Tan <btan400@gmail.com>
2025-08-14 17:24:40 +1000

    2-arguments.js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+num_args = process.argv.length;
+num_args = num_args - 2;
+
+if (num_args < 1) {
+    console.log("No argument")
+}
+else if (num_args === 1) {
+    console.log("Argument found")
+}
+else if(num_args > 1) {
+    console.log("Arguments found")
+};
