6985af788b23882a7f9f2cf7d76d2dec1d425b99
Bevan Tan <btan400@gmail.com>
2025-08-16 13:50:36 +1000

    semistandard --fix

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -4,9 +4,9 @@ num_args = process.argv.length;
 num_args = num_args - 2;
 
 if (num_args < 1) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (num_args === 1) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else if (num_args > 1) {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
