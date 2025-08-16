8948617a7532d994cb3b6fa7712251e8801c96a2
Bevan Tan <btan400@gmail.com>
2025-08-16 14:32:48 +1000

    4-concat.js

---
+++ javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// 4. Create a sentence
+// Write a script that prints two arguments passed to it, in the following format: “ is ”
+
+const arg = process.argv;
+console.log(arg[2] + " is " + arg[3]);
