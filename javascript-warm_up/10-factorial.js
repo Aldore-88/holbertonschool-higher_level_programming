ca0c89a6cb3493740ac8bf19277cf0f25979c4a4
Bevan Tan <btan400@gmail.com>
2025-08-16 15:56:01 +1000

    10-factorial.js

---
+++ javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial(num){
+    if (num === 0 || num === 1 || isNaN(num)){
+        return 1;
+    }
+    // console.log(num)
+    return (num * factorial(num - 1));
+}
+
+inputNum = parseInt(process.argv[2])
+// console.log(inputNum)
+console.log(factorial(inputNum))
