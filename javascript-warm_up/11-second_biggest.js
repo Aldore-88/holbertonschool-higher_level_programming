d2728c6d7c33fdea86426a53228ab9956d59c6d0
Bevan Tan <btan400@gmail.com>
2025-08-17 21:16:01 +1000

    11-second_biggest.js

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 
 const onlyNums = process.argv.slice(2);
-
-console.log(onlyNums);
-console.log(onlyNums.sort((a, b) => b - a));
+if (onlyNums.length > 1) {
+  // console.log(onlyNums);
+  console.log(onlyNums.sort((a, b) => b - a));
+  console.log(onlyNums[1]);
+} else {
+  console.log('0');
+}
