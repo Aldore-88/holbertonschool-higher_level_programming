55a7c7a3bbf04a6623411c7b4a1dab99f29ce53c
Bevan Tan <btan400@gmail.com>
2025-08-17 21:17:14 +1000

    removed extra print

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 const onlyNums = process.argv.slice(2);
 if (onlyNums.length > 1) {
   // console.log(onlyNums);
-  console.log(onlyNums.sort((a, b) => b - a));
+  onlyNums.sort((a, b) => b - a);
   console.log(onlyNums[1]);
 } else {
   console.log('0');
